8bf3a6588f73e1681e085f605a04680549fd81c5
PAWAN JENU <pawanjenu@gmail.com>
2022-01-02 22:03:21 +0530

    add (Leetcode-Medium): 684

--- Leetcode/Medium/684._Redundant_Connection/Solution.java
+++ Leetcode/Medium/684._Redundant_Connection/Solution.java
@@ -1,13 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertSame;
-
 class Solution {
 
     static final File ip = new File("input.txt");
@@ -39,81 +39,53 @@ class Solution {
 
     }
 
-    public int findCircleNum2(int[][] isConnected) {
-        // BFS approach
-        int n = isConnected.length;
-        int[] visited = new int[n];
-        int count = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++){
-            if (visited[i] == 0){
-                queue.add(i);
-                while (!queue.isEmpty()){
-                    int node = queue.remove();
-                    visited[node] = 1;
-                    for (int j = 0; j < n; j++){
-                        if (isConnected[node][j] == 1 && visited[j] == 0){
-                            queue.add(j);
-                        }
-                    }
-                }
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public void dfs(int[][] isConnected, int[] visited, int node){
-        for (int j = 0; j < isConnected.length; j++){
-            if (isConnected[node][j] == 1 && visited[j] == 0){
-                visited[j] = 1;
-                dfs(isConnected, visited, j);
-            }
-        }
-    }
-    public int findCircleNum3(int[][] isConnected) {
-        // DFS approach
-        int count = 0;
-        int n = isConnected.length;
-        int[] visited  = new int[n];
-        for (int i = 0; i < isConnected.length; i++){
-            if (visited[i] == 0){
-                visited[i] = 1;
-                dfs(isConnected, visited, i);
-                count++;
-            }
-        }
-        return count;
-    }
-
     public int find(int[] parent, int node){
         return parent[node] == 0 ? node : find(parent, parent[node]);
     }
-    public void union(int[] parent, int x, int y){
+    public boolean union(int[] parent, int[] rank, int x, int y){
         int xpar = find(parent, x);
         int ypar = find(parent, y);
 
-        if (xpar != ypar){
+        if (xpar == ypar){
             parent[xpar] = ypar;
+            return false;
+        }
+        else if(rank[xpar] > rank[ypar]){
+            parent[xpar] = ypar;
+            return true;
+        }
+        else {
+            parent[ypar] = xpar;
+            return true;
         }
     }
-    public int findCircleNum(int[][] isConnected) {
-        int count = 0;
-        int n = isConnected.length;
-        int[] parent = new int[n];
-        Arrays.fill(parent, -1);
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (isConnected[i][j] == 1 && i != j){
-                    union(parent, i, j);
-                }
+    public int[] findRedundantConnection(int[][] edges) {
+        int max_nodes = 1001;
+        int[] parent = new int[max_nodes];
+        int[] rank = new int[max_nodes];
+
+        for (int[] edge: edges){
+            if (!union(parent, rank, edge[0], edge[1]))
+                return edge;
+        }
+        throw new AssertionError();
+    }
+
+    public int[] findRedundantConnection1(int[][] edges) {
+        // small version
+        int max_nodes = 1001;
+        int[] parent = new int[max_nodes];
+        for (int[] edge: edges){
+            int x = edge[0], y = edge[1];
+            int xpar = find(parent, x);
+            int ypar = find(parent, y);
+            if (xpar == ypar)
+                return edge;
+            else {
+                parent[xpar] = ypar;
             }
         }
-        for (int i = 0; i < n; i++){
-            if (parent[i] == -1)
-                count++;
-        }
-        return count;
+        throw new AssertionError();
     }
     public static void main(String[] args) throws FileNotFoundException, IOException {
         solve();
@@ -268,14 +240,14 @@ class Solution {
 
     @ParameterizedTest
     @MethodSource("generateData")
-    public void testSolution(int[][] nums, int output) {
-        int result = new Solution().findCircleNum(nums);
-        assertSame(output, result);
+    public void testSolution(int[][] nums, int[] output) {
+        int[] result = new Solution().findRedundantConnection(nums);
+        Assertions.assertArrayEquals(output, result);
     }
 
     static Stream<Arguments> generateData() {
         return Stream.of(
-                Arguments.of(new int[][]{{1,1,0}, {1,1,0}, {0,0,1}}, 2)
+                Arguments.of(new int[][]{{1,2}, {1,3}, {2,3}},new int[]{2,3})
         );
     }
 }
